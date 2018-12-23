6620637d7a0c9217bb9c4fa13ff2e8c3f42c376b
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2018-12-23 10:15:26 -0500

    Creacion de un Chat simple con la libreria socket.io

---
+++ app.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+
+const server = http.createServer(app);
+
+// configuración del Servidor
+app.set('port', 3000);
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
+
+server.listen(app.get('port'), function () {
+    console.log('servidor en puerto 3000');
+});
+
+require('./sockets')(server);
---
+++ sockets.js
@@ -0,0 +1,15 @@
+const io = require('socket.io');
+
+module.exports = function(server) {
+    var sockets = io.listen(server);
+
+    sockets.on('connection', function(socket) {
+
+        console.log('Nuevo Usuario conectado');
+
+        socket.on('mensaje-del-cliente', function(data) {
+            sockets.emit('mensaje-del-servidor', data);
+        });
+
+    });
+}
